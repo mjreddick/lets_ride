624d7b867ce65b707900d344480a7310b0fe5b8b
Mia VanValkenburg <mia.vanvalkenburg@gmail.com>
2015-01-23 14:40:32 -0800

    add static_pages controller and root

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :user do
     email "MyString"
-first_name "MyString"
-last_name "MyString"
-password_digest "MyString"
+		first_name "MyString"
+		last_name "MyString"
+		password_digest "MyString"
   end
 
 end
