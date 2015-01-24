82acc355202e8143aa8bf4003f167a87ea7b4c36
Mia VanValkenburg <mia.vanvalkenburg@gmail.com>
2015-01-24 09:46:19 -0800

    add nav-bar, updated routes, basic styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   
-  get 'user/index'
-  get 'login' => 'sessions#new'
-  post 'login' => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
+  resources :users
+  
+  get "/signup" => "users#new"
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
