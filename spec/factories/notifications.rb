6d11d68095e80000925ed3cdaaa92c6e1cb5c531
Matthew Reddick <matthewjreddick@gmail.com>
2015-01-27 00:13:56 -0800

    define factories for models

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :notification do
     type 1
+    user
+    request
   end
 
 end
--- spec/factories/userrides.rb
+++ spec/factories/userrides.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
   factory :userride do
-    user_address ""
-is_driver ""
+    user_address "1617 Broadway, Santa Monica, CA"
+		is_driver false
+		user
+		ride
   end
 
 end
