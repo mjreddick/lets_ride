1b3fbdef2f75a10e2725e4c85f22616678765a99
ponlok huot <phuot1980@gmail.com>
2015-01-26 19:20:36 -0800

    created routes, view, def index in controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   
   resources :users
-  
+  resources :rides
+
   get "/signup" => "users#new"
   get "/login" => "sessions#new"
   post "/login" => "sessions#create"
