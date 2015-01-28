b659b658f31b33d19c53fdc4acda6c91d324e496
ponlok huot <phuot1980@gmail.com>
2015-01-27 18:48:40 -0800

    defined user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,16 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
 		if @user.save
-			redirect_to @user
+			redirect_to @user, notice: "user saved"
 		else
 			#redisplay the form that is created. 
-			render 'new'
+			render 'new', alert: "something was wrong"
 		end
   end
+
+  def edit
+		@submit_message = 'Edit Account'
+	end
 
   private	
 
