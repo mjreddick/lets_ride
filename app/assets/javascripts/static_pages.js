7b3faa77289bbf4bdae4e40bbffc804f51405137
Matthew Reddick <matthewjreddick@gmail.com>
2015-01-27 20:47:55 -0800

    add some styling to homepage search results

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -4,7 +4,7 @@
 
 
 $(function() {
-	$('#search-button').on('click', sendSearch)
+	$('#search-button').click(sendSearch)
 });
 
 function sendSearch() {
@@ -15,7 +15,7 @@ function sendSearch() {
 	    keywords: keywords,
 	    location: location,
 	    date: "Future",
-	    image_sizes: ["block200"],
+	    image_sizes: ["medium"],
 	    page_size: 6,
 	    page_number: 1
 		};
@@ -36,22 +36,41 @@ function searchResultsReceived(eventData) {
 	
 	for(var i = 0; i < eventArray.length; i++) {
 		container.append(createEventBox(eventArray[i], i));
-		// container.append(createEventModal(eventArray[i], i))
+		container.append(createEventModal(eventArray[i], i))
 	}
 }
 
 function createEventBox(event, i){
-	var s;
-	try {
-		s = '<div class="col-md-4"><img src="'+ event.image.block200.url +'"></div>'; 
+	var eventBox = '<div class="col-md-4"><div class="event-box" data-toggle="modal" data-target="';
+	// add the modal name for this event
+	eventBox += '#modal_' + i + '">';
+	eventBox += '<img  class="event-image img-responsive" width="128" height="128" src="';
+	// add the image provided or a default image if one isn't provided
+	if(event.image && event.image.medium && event.image.medium.url) {
+		eventBox += 	event.image.medium.url;
 	}
-	catch (e) {
-		s = '<div class="col-md-4"></div>'; 
+	else {
+		eventBox += "http://www.homelesshouston.org/wp-content/uploads/2012/09/blank_test-200x200.png";
 	}
-	return s;
+
+	eventBox += '""><div class="event-box-text">';
+	eventBox += '<p>' + event.title + '</p>';
+	eventBox += '<p>' + event.venue_name + '</p>';
+	eventBox += '<p>' + event.start_time + '</p>';
+	eventBox += '</div></div></div>';
+	
+	return eventBox;
 	
 }
 
 function createEventModal(event, i){
-
+	var modal = '<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" id="';
+	// add modal name for this event
+	modal += 'modal_' + i + '">';
+	modal += '<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">';
+	modal += event.title + '</h4></div><div class="modal-body">';
+	// add modal content
+	modal += 'Some Stuff';
+	modal += '</div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button><button type="button" class="btn btn-primary">Save changes</button></div></div></div></div>';
+	return modal;
 }
