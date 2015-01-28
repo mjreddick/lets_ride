901945dc28be48a616704c6cf398e061e8787686
ponlok huot <phuot1980@gmail.com>
2015-01-27 17:41:32 -0800

    add create user crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,22 @@ class UsersController < ApplicationController
     @user = User.new
     @submit_message = 'Sign Up'
   end
-  
+
+  def create
+  	@user = User.new(user_params)
+		if @user.save
+			redirect_to @user
+		else
+			#redisplay the form that is created. 
+			render 'new'
+		end
+  end
+
+  private	
+
+  def user_params
+  	params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :image)
+  end
+
 end
 
