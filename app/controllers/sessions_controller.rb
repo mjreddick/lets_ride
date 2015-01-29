b9ab74b16d0ad2a8e679b513b982c83e4aea9d39
Matthew Reddick <matthewjreddick@gmail.com>
2015-01-29 09:43:20 -0800

    fix error in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
 	def create
-  	user = User.where(email: params[:session][:email].downcase, is_active: true).first
+  	user = User.where(email: params[:session][:email].downcase).first
   	if user && user.authenticate(params[:session][:password])
  			log_in user
  			redirect_to user
