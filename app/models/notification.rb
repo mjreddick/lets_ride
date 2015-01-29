7d76b253950622855ae668898710b1fe0bc6db7c
Mia VanValkenburg <mia.vanvalkenburg@gmail.com>
2015-01-28 20:36:41 -0800

    add validations and type setters

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,6 +2,26 @@ class Notification < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :request
 
+	validates :user, presence: true
+	validates :request, presence: true
 	validates :type, presence: true, numericality: { only_integer: true, 
 		greater_than_or_equal_to: 0, less_than_or_equal_to: 3 }
+
+  
+  def set_pending
+  	self.type = 2 
+  end
+	
+	def set_accepted
+		self.type = 1
+	end
+
+	def set_rejected
+		self.type = 0
+	end
+
+	def set_approval
+		self.type = 3
+	end
+
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -2,4 +2,7 @@ class Request < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :ride
 	has_many :notifications
+
+	validates :user, presence: true
+	validates :ride, presence: true
 end
