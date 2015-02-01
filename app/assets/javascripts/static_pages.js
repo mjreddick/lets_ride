41d422ca481cdf7334be7430fd48e0cdd94edcb9
Matthew Reddick <matthewjreddick@gmail.com>
2015-02-01 13:25:34 -0800

    add input fields for creating event, refactor javascript code

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -3,6 +3,7 @@
  // You can use CoffeeScript in this file: http://coffeescript.org/
 
 var DEFAULT_IMAGE_URL = 'https://s3-us-west-1.amazonaws.com/lets-ride/default-icon.png';
+var events = [];
 
 $(function() {
 	$('#search-button').click(sendSearch)
@@ -40,6 +41,8 @@ function sendSearch() {
 }
 
 function searchResultsReceived(eventData) {
+	// clear old events
+	events = [];
 	// enable button again
 	$('#search-button').removeClass("disabled")
 	$('#loading-spinner').hide();
@@ -50,34 +53,60 @@ function searchResultsReceived(eventData) {
 	var eventArray = eventData.events.event;
 	console.log(eventArray)
 	
+	// put the search results in an easy to use format
+	for(var i =0; i < eventArray.length; i++){
+		var event = {};
+		var currentEvent = eventArray[i];
+		
+		event.title = currentEvent.title;
+		event.details = currentEvent.description;
+		event.starDateTime = currentEvent.start_time;
+
+		// format the date_time for easy use
+		var dateTime = new Date(event.starDateTime);
+		event.formattedDateTime = dateTime.toDateString();
+
+		event.venueName = currentEvent.venue_name;
+		event.id = currentEvent.id;
+
+		if(currentEvent.image && currentEvent.image.medium && currentEvent.image.medium.url) {
+			event.mediumImage = 	currentEvent.image.medium.url;
+		}
+		else {
+			event.mediumImage = DEFAULT_IMAGE_URL;
+		}
+
+		if(currentEvent.image && currentEvent.image.large && currentEvent.image.large.url) {
+			event.largeImage = 	currentEvent.image.large.url;
+		}
+		else {
+			event.largeImage = DEFAULT_IMAGE_URL;
+		}
+
+		events.push(event);
+
+	}
+
 	for(var i = 0; i < eventArray.length; i++) {
-		container.append(createEventBox(eventArray[i], i));
-		container.append(createEventModal(eventArray[i], i))
+		container.append(createEventBox(events[i], i));
+		container.append(createEventModal(events[i], i))
 	}
 	$('.drive-btn').click(chooseDrive);
 	$('.ride-btn').click(chooseRide);
 }
 
 function createEventBox(event, i){
-	// get the date/time
-	var date_time = new Date(event.start_time);
-	date_time = date_time.toDateString();
 
 	var eventBox = '<div class="col-sm-4"><div class="event-box" data-toggle="modal" data-target="';
 	// add the modal name for this event
 	eventBox += '#modal_' + i + '">';
 	eventBox += '<img  class="event-image img-responsive" width="128" height="128" src="';
-	// add the image provided or a default image if one isn't provided
-	if(event.image && event.image.medium && event.image.medium.url) {
-		eventBox += 	event.image.medium.url;
-	}
-	else {
-		eventBox += DEFAULT_IMAGE_URL;
-	}
+
+	eventBox += event.mediumImage;
 
 	eventBox += '"><div class="event-box-text">';
 	eventBox += '<p><span class="event-box-title">' + formattedTitle(event.title) + '</span></p>';
-	eventBox += '<p>' + date_time + '</p>';
+	eventBox += '<p>' + event.formattedDateTime + '</p>';
 	eventBox += '</div></div></div>';
 	
 	return eventBox;
@@ -85,32 +114,26 @@ function createEventBox(event, i){
 }
 
 function createEventModal(event, i){
-	// get the date/time
-	var date_time = new Date(event.start_time);
-	date_time = date_time.toDateString();
 
 	var modal = '<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" id="';
 	// add modal name for this event
 	modal += 'modal_' + i + '">';
 	modal += '<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">';
 	modal += '<strong>' + event.title + '</strong></h4></div><div class="modal-body">';
-	// add hidden event info to be used to create ride
+	
+	// add hidden event info to be used to create rides
+	modal += '<div class="hidden event-index">' + i + '</div>';
 
-	modal += '<div class="hidden event-eventful-id">' + event.id + '</div>';
 	// add modal content
+	// add event picture
 	modal += '<img  class="large-event-image img-responsive" width="480" height="480" src="';
-	// add the image provided or a default image if one isn't provided
-	if(event.image && event.image.large && event.image.large.url) {
-		modal += 	event.image.large.url;
-	}
-	else {
-		modal += DEFAULT_IMAGE_URL;
-	}
+	modal += event.largeImage;
 	modal += '">';
-	modal += '<p>' + event.venue_name + '</p>';
-	modal += '<p>' + date_time + '</p>';
-	if(event.description) {
-		modal += '<p>' + event.description + '</p>';
+	
+	modal += '<p>' + event.venueName + '</p>';
+	modal += '<p>' + event.formattedDateTime + '</p>';
+	if(event.details) {
+		modal += '<p>' + event.details + '</p>';
 	}
 	modal += '</div><div class="modal-footer text-center"><button type="button" class="btn btn-default btn-lg ride-btn">Ride</button><button type="button" class="btn btn-primary btn-lg drive-btn">Drive</button></div></div></div></div>';
 	return modal;
@@ -142,10 +165,18 @@ function chooseHelper(self){
 		// hide the modal
 		modal.modal('hide');
 
-		var id = modal.find('.event-eventful-id').html();
+		// get the event associated with the modal
+		var index = modal.find('.event-index').html();
+		var event = events[index];
 
-		// console.log(id)
-		$('#ride_eventful_id').val(id);
+		// insert all the needed event values into the create ride modal
+		$('#ride_eventful_id').val(event.id);
+		$('#ride_image_url_medium').val(event.mediumImage);
+		$('#ride_image_url_large').val(event.largeImage);
+		$('#ride_title').val(event.title);
+		$('#ride_details').val(event.details);
+		$('#ride_start_date_time').val(event.starDateTime);
+		$('#ride_venue_name').val(event.venueName);
 
 		return true;	
 	}
@@ -159,7 +190,10 @@ function chooseHelper(self){
 // after a user says they want a ride and
 // inputs a zipcode
 function lookForRides(){
+	// get the zipcode from the zipcode modal
 	var zipcode = $('#zipcodeInput').val();
+
+	// get the eventful_id from the new ride modal
 	var eventId = $('#ride_eventful_id').val();
 	window.location.href = "/rides?event_ful_id=" + eventId + 
 	"&zipcode=" + zipcode;
