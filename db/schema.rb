0156ab9b6e2f19cd540b50b38d697a6e431ca688
Matthew Reddick <matthewjreddick@gmail.com>
2015-02-01 12:55:21 -0800

    add venue_name to event model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150201191855) do
+ActiveRecord::Schema.define(version: 20150201205340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150201191855) do
     t.string   "image_url_medium"
     t.string   "title"
     t.string   "start_date_time"
+    t.string   "venue_name"
   end
 
   create_table "notifications", force: true do |t|
