848f6f4e9e90c6785cf35d1bccdc3ddd8deff23e
Mia VanValkenburg <mia.vanvalkenburg@gmail.com>
2015-02-02 08:19:33 -0800

    passing specs

--- spec/controllers/rides_controller_spec.rb
+++ spec/controllers/rides_controller_spec.rb
@@ -1,16 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe RidesController, :type => :controller do
-  describe "GET #index" do
+
+  describe "Create new ride" do
     it "responds successfully with an HTTP 200 status code" do
-      get :index
+      get :new
       expect(response).to be_success
       expect(response).to have_http_status(200)
-    end
-
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template("index")
     end
   end
 
