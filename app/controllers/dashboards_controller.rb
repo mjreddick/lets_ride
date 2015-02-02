7cea8a8d69eac0127da52d510020870096e9a913
Matthew Reddick <matthewjreddick@gmail.com>
2015-02-01 17:37:14 -0800

    add check for correct user to dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,16 @@
 class DashboardsController < ApplicationController
-
+	before_action :require_correct_user, only: [:show]
 	def show
+		@notifications = current_user.notifications
+
 	end
 	
+	private
+		def require_correct_user
+			unless logged_in_as(params[:id])
+				flash[:danger] = "You must be logged in to complete that action"
+				redirect_to :login and return
+			end
+		end
+
 end
